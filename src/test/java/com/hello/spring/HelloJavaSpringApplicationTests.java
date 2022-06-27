b0461c1a7eaab54c394f8c7573694509b9078483
Management Trainee Technology Head Office (Juan) <095085@bfi.co.id>
2022-06-27 17:17:51 +0700

    first commit

---
+++ src/test/java/com/hello/spring/HelloJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.spring;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HelloJavaSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
